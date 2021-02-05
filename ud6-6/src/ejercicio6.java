b22b4bbcb1c034232333caf7d704fcee41510e58
Sergio <sergio_castilla_13@hotmail.com>
2021-02-05 19:05:05 +0100

    ejercicio6

--- ud6-6/src/ejercicio6.java
+++ ud6-6/src/ejercicio6.java
@@ -5,11 +5,15 @@ public class ejercicio6 {
 		// TODO Auto-generated method stub
 		int n = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número natural: "));
 		while(n<0) n = Integer.parseInt(JOptionPane.showInputDialog("No has introducido un número natural. Por favor, introduce un número natural: "));
+		
+		System.out.println("El número introducido contiene "+cuentaCifras(n)+" cifras.");
+	}
+	public static int cuentaCifras(int n) {
 		int c= 0;
 		while(n>0) {
 			n/=10;
 			c++;
 		}
-		System.out.println("El número introducido contiene "+c+" cifras.");
+		return c;
 	}
 }
