0abf2a33640c2dceaff5216687f8792409690a5b
Sergio <sergio_castilla_13@hotmail.com>
2021-02-08 16:14:19 +0100

    ejercicio11

---
+++ ud6-11/src/ejercicio11.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+import java.util.Random;
+public class ejercicio11 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t = Integer.parseInt(JOptionPane.showInputDialog("Introduce el tamaño de los arrays: "));
+		int n[] = new int[t];
+		int n2[] = n; //Apunta al array anterior por lo que lo tratará como un puntero y sobreescibirá sus datos si se intenta modificar
+		inValores(n,t);
+		inValores(n2,t);//El resultado serán los cuadrados de ésta generación de datos.
+		int res[] = multiplica(n,n2);
+		for(int i=0;i<res.length;i++) {
+			System.out.println("Posición "+i+" de Cada Array:");
+			System.out.println("Array1: "+n[i]+" Array2: "+n2[i]+" Array3: "+res[i]);
+		}
+	}
+	public static void inValores(int[] n, int t) {
+		for(int i=0;i<t;i++) {
+			n[i] = new Random().nextInt(10);
+		}
+	}
+	public static int[] multiplica(int[] n, int[] n2) {
+		int[] mult = new int[n.length];
+		for(int i=0;i<n.length;i++) {
+			mult[i]=n[i]*n2[i];
+		}
+		return mult;
+	}
+}
